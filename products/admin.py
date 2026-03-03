a06d742a468349ac52aa0b0d649395850bd85398
Ayush <ayush-devhub@gmail.com>
2026-03-03 11:36:23 +0530

    chore: register models in admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ("name", "price", "is_active", "created_at")
+    list_filter = ("is_active", "created_at")
